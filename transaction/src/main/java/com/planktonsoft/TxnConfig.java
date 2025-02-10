93dd7779c4e9245a79307eb5d747a71cf80afc2b
Santi Lertsumran <santi.lertsumran@gmail.com>
2025-02-10 10:28:21 +0700

    introduce env for kafka config

--- transaction/src/main/java/com/planktonsoft/TxnConfig.java
+++ transaction/src/main/java/com/planktonsoft/TxnConfig.java
@@ -8,11 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
 @Configuration
 public class TxnConfig {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
 
     @Bean
     RestTemplate getTemplate(){
@@ -21,7 +26,8 @@ public class TxnConfig {
 
     Properties getCProperties(){
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers
+        );
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return properties;
@@ -29,7 +35,8 @@ public class TxnConfig {
 
     Properties getPProperties(){
         Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers
+        );
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
