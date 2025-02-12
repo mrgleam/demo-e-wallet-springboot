e9032dcd31de05f580ed0879c9181f10101428b6
Santi Lertsumran <santi@odds.team>
2025-02-12 17:50:28 +0700

    refactor: remove unused import package

--- pocket/src/main/java/com/planktonsoft/PocketConfig.java
+++ pocket/src/main/java/com/planktonsoft/PocketConfig.java
@@ -1,6 +1,5 @@
 package com.planktonsoft;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
